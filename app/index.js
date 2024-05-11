f7e7bbaa9431d50e6980d16ad41e9d5a1e611bbf
Teo <huyteo1003@gmail.com>
2024-05-11 22:55:42 +0700

    base code

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Sta() {
+  return (
+    <View className="bg-red-200 pt-20" >
+      <Text className="text-3xl text-center">Home</Text>
+    </View>
+  )
+}
