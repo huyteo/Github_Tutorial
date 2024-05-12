32718dd9a9066d6aac6cfbfbf96dcde3045de162
Teo <huyteo1003@gmail.com>
2024-05-12 20:49:30 +0700

    signIn.js phút thứ 32.10

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import {Slot} from "expo-router";
+import React, { useEffect } from 'react'
+import {Slot, useRouter, useSegments} from "expo-router";
 import "../global.css";
+import { AuthContextProvider, useAuth } from './context/authContext';
 
-export default function _layout() {
+const MainLayout = ()=>{
+  const {isAuthenticated} = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+
+  useEffect(()=>{
+    // check if user is authenticated or not
+    if(typeof isAuthenticated=='undefined') return;
+    const inApp = segments[0]=='(app)';
+    if(isAuthenticated && !inApp) {
+        //redirect to home
+        router.replace('home')
+    }else if(isAuthenticated==false){
+      // redirect to signin
+      router.replace('signIn');
+    }
+  },[isAuthenticated])
+
+  return <Slot/>
+}
+
+export default function Rootlayout() {
   return (
-    <View>
-      <Slot />
-    </View>
+    <AuthContextProvider>
+      <MainLayout/>
+    </AuthContextProvider>
   )
 }
--- app/signIn.js
+++ app/signIn.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { StatusBar } from 'expo-status-bar';
 
 export default function signIn() {
   return (
-    <View>
-      <Text>signIn</Text>
+    <View className="flex-1">
+        <StatusBar style='dark'/>
+        <View className="flex-1 gap-12">
+          {/* signIn image */}
+          
+          <View className="items-center">
+            <Image style={{height: hp(25)}} resizeMode='contain' source={require('../assets/images/login.png')}/>
+          </View>
+        </View>
     </View>
   )
 }
