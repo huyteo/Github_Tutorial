4abaa5ddcbdf2e6a60478a119bd329fad03aeb5a
Teo <huyteo1003@gmail.com>
2024-05-16 21:26:41 +0700

    1:58:23

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,59 +1,24 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-export default function ChatItem({ item }) {
-  return (
-    <TouchableOpacity style={styles.container}>
-      <Image 
-        source={require('../assets/images/avatar.png')} 
-        style={styles.avatar} 
-      />
+export default function ChatItem({ item, router, noBorder }) {
+return (
+<TouchableOpacity className="flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 border-b border-neutral-200">
+<Image
+source={require('../assets/images/avatar.png')}
+style={{ height: hp(10), width: hp(10), borderRadius: hp(10) }}
+/>
 
-      {/* name and last message */}
-      <View style={styles.detailsContainer}>
-        <View style={styles.rowContainer}>
-          <Text style={styles.nameText}>Nomi</Text>
-          <Text style={styles.timeText}>Time</Text>
-        </View>
-      </View>
-    </TouchableOpacity>
-  );
+
+  {/* name and lát mesage */}
+  <View className="flex-1 gap-1">
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+      <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-800">Nomi</Text>
+      <Text style={{fontSize: hp(1.6)}} className="font-medium text-neutral-500">Time</Text>
+    </View>
+    <Text style={{fontSize: hp(1.6)}} className="font-medium text-neutral-500">Last message</Text>
+  </View>
+</TouchableOpacity>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginHorizontal: wp(4),
-    marginBottom: hp(4),
-    paddingBottom: hp(2),
-    borderBottomColor: 'lightgray',
-    borderBottomWidth: 1,
-  },
-  avatar: {
-    height: hp(10),
-    width: hp(10),
-    borderRadius: hp(10),
-  },
-  detailsContainer: {
-    flex: 1,
-    marginLeft: 10,
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  nameText: {
-    fontSize: hp(1.8),
-    fontWeight: 'bold',
-    color: 'gray',
-  },
-  timeText: {
-    fontSize: hp(1.6),
-    fontWeight: 'normal',
-    color: 'gray',
-  },
-});
--- components/ChatList.js
+++ components/ChatList.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { View, Text, FlatList } from 'react-native';
 import ChatItem from './ChatItem';
+import { useRoute } from '@react-navigation/native';
+import { useRouter } from 'expo-router';
 
 export default function ChatList({ users }) {
+  const router = useRouter();
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         data={users}
         contentContainerStyle={{ flex: 1, paddingVertical: 25 }}
-        keyExtractor={(item) => item.id || Math.random().toString()} // Use a unique identifier (if available)
+        keyExtractor={(item) => item.id || Math.random().toString()}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <ChatItem image={item.image} text={item.text} />}
+        renderItem={({ item, index }) => 
+          <ChatItem
+            noBorder={index + 1 === users.length} // Sử dụng index được truyền từ renderItem
+            router={router}
+            item={item}
+            index={index}
+          />
+        }
       />
     </View>
   );
